879533f60b2ea8306dbdeb7b07c368bf53241436
Anastasia <nastiakarachova@gmail.com>
2021-06-15 19:26:54 +0300

    added redirection to the git repository

--- src/components/RepositoryList/RepositoryItem/RepositoryItem.tsx
+++ src/components/RepositoryList/RepositoryItem/RepositoryItem.tsx
@@ -6,18 +6,18 @@ interface RepositoryItemProps {
     list: {
         name: string,
         language: string,
-        description: string
+        description: string,
+        html_url: string
     }
 }
 
 const RepositoryItem = ({list}: RepositoryItemProps) => {
     return (
-        <div className="repository__item">
-            <span className="repository__item__title">{list.name}</span>
-            <span className="repository__item__heading">Language: <span className="repository__item__value">{list.language}</span></span>
-            <span className="repository__item__heading">Description: <span className="repository__item__value">{list.description}</span></span>
-        </div>
-
+            <a href={list.html_url} target="_blank" className="repository__item" rel="noopener noreferrer">
+                <span className="repository__item__title">{list.name}</span>
+                <span className="repository__item__heading">Language: <span className="repository__item__value">{list.language}</span></span>
+                <span className="repository__item__heading">Description: <span className="repository__item__value">{list.description}</span></span>
+            </a>
     )
 }
 export default RepositoryItem
--- src/types/listTypes.ts
+++ src/types/listTypes.ts
@@ -2,6 +2,7 @@ interface repositoryItem{
     name: string,
     language: string
     description: string,
+    html_url: string
 }
 
 export interface listState{
